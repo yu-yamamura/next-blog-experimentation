a568568f0191d44be2d7af22082c1e404bdfa7d8
yamamurc <90120750+yamamurc@users.noreply.github.com>
2022-05-16 05:42:05 +0900

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,77 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+  },
+  plugins: [
+    '@typescript-eslint',
+    'import',
+    'unused-imports',
+    'jest',
+    'testing-library',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'next/core-web-vitals',
+    'plugin:jest/recommended',
+    'plugin:jest/style',
+    'plugin:testing-library/react',
+    'prettier',
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'import/newline-after-import': ['error'],
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'type',
+          ['parent', 'sibling'],
+          'index',
+          'object',
+        ],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        pathGroups: [
+          {
+            pattern: '@/lib/**',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '@/hooks/**',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '@/components/**',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '@/types/**',
+            group: 'type',
+            position: 'before',
+          },
+          {
+            pattern: '@/styles/**',
+            group: 'object',
+            position: 'after',
+          },
+        ],
+      },
+    ],
+    'jest/consistent-test-it': ['warn', { fn: 'test' }],
+  },
+};
